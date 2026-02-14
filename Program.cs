ced10b0253e932356eda4981426a556161091341
Justin <cabrerajustin13@gmail.com>
2026-02-14 12:04:06 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace AccountManagementAlatiit
+  
 {
+    using System;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSYEM");
+
+            string username = "karlopogi";
+
+            string password = "karlolangmalakas123!";
+
+            Console.Write("Enter username: ");
+            string inputUser = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string inputPass = Console.ReadLine();
+
+
+            if (inputUser == username && inputPass == password) {
+                Console.WriteLine("Login successfull"); 
+                }
+            else { 
+               Console.WriteLine("Incorrect credentials.");
         }
     }
 }
+}
+
+   
